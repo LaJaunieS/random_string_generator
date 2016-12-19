fee96c63d0c7d3b090d5e8165d74c160fa5648ec
LaJaunieS <LaJaunieS@users.noreply.github.com>
2016-12-19 13:45:26 -0800

    General cleanup

--- app.js
+++ app.js
@@ -2,88 +2,83 @@
 
 console.log('script running');
 
-const characters = {
-	lcLettersArray: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ],
-	ucLettersArray: [],
-	symbolsArray: [ '!', '-', '^', '?', '&', '*', '#'],
-};
-
-function getRandomLcLetter() {
-		let num = randomObject.randomInt(0, characters.lcLettersArray.length-1);
-		let lcLetter = characters.lcLettersArray[num];
-    	randomObject.finalArray.push(lcLetter);
-		};
-function getRandomUcLetter() {
-        let num = randomObject.randomInt(0, characters.ucLettersArray.length-1);
-		let ucLetter = characters.ucLettersArray[num];
-        randomObject.finalArray.push(ucLetter);
-        };
-function getRandomSymbol() {
-        let num = randomObject.randomInt(0, characters.symbolsArray.length-1);
-		let symbol = characters.symbolsArray[num];
-        randomObject.finalArray.push(symbol);
-        };
-function getRandomNumber(){
-        let num = randomObject.randomInt(0, 9);
-		let number = num;
-		randomObject.finalArray.push(number);
-        };
-
-
-let randomObject = {
-	finalArray: [],
-	pwLength: 10,
-    randomInt: function(min, max) {
-                      min = Math.ceil(min);
-                      max = Math.floor(max);
-                      return Math.floor(Math.random() * (max - min + 1)) + min;
-                    },
-    
-    arrayOfFunctions: [ getRandomLcLetter, getRandomUcLetter, getRandomSymbol, getRandomNumber ]
-};
-
-characters.lcLettersArray.forEach(function(item) {
-	characters.ucLettersArray.push(item.toUpperCase());
-	});
-
-
-const regExes = {
-    lcRegExp: /[a-z]+/,
-    ucRegExp: /[A-Z]+/,
-    numRegExp: /[0-9]+/,
-    symbolRegExp: /(\!+)|(\-+)|(\^+)|(\?+)|(\&+)|(\*+)|(\#+)/
-};
-
-
-
 function buildString() {
-        let pw = randomObject.pwLength;
-        //let enoughChars = randomObject.pwArray.length;
-        let allConditionsMet = false;
-        let yourString = '';
-        
-    allConditionsMet = regExes.lcRegExp.test(yourString) && regExes.ucRegExp.test(yourString) && regExes.numRegExp.test(yourString) && regExes.symbolRegExp.test(yourString);
-    
-    
-    while (allConditionsMet === false) {
-        randomObject.finalArray= [];
-            for(var i = 0; i < pw; i++) {
-                let x = randomObject.randomInt(0,3);
-                randomObject.arrayOfFunctions[x]();
-                yourString = randomObject.finalArray.join('');
-                allConditionsMet = regExes.lcRegExp.test(yourString) && regExes.ucRegExp.test(yourString) && regExes.numRegExp.test(yourString) && regExes.symbolRegExp.test(yourString);
-                
-            };       
-        yourString = randomObject.finalArray.join('');
-        console.log(yourString);
-        
-                console.log(allConditionsMet);
-                console.log(yourString);
-        };
-                
 
-   }; //exit buildString function 
-	
+    const characters = {
+        lc: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ],
+        uc: [],
+        symbols: [ '!', '-', '^', '?', '&', '*', '#'],
+    };
+    
+     const regExes = {
+        lcRegExp: /[a-z]+/,
+        ucRegExp: /[A-Z]+/,
+        numRegExp: /[0-9]+/,
+        symbolRegExp: /(\!+)|(\-+)|(\^+)|(\?+)|(\&+)|(\*+)|(\#+)/
+    };
+
+    function getRandomLcLetter() {
+            let num = randomObject.randomInt(0, characters.lc.length-1);
+            let lcLetter = characters.lc[num];
+            randomObject.finalArray.push(lcLetter);
+            };
+    function getRandomUcLetter() {
+            let num = randomObject.randomInt(0, characters.uc.length-1);
+            let ucLetter = characters.uc[num];
+            randomObject.finalArray.push(ucLetter);
+            };
+    function getRandomSymbol() {
+            let num = randomObject.randomInt(0, characters.symbols.length-1);
+            let symbol = characters.symbols[num];
+            randomObject.finalArray.push(symbol);
+            };
+    function getRandomNumber(){
+            let num = randomObject.randomInt(0, 9);
+            let number = num;
+            randomObject.finalArray.push(number);
+            };
+
+
+    let randomObject = {
+        finalArray: [],
+        pwLength: 8,  //this will eventually be set by the user
+        randomInt: function(min, max) {
+                          min = Math.ceil(min);
+                          max = Math.floor(max);
+                          return Math.floor(Math.random() * (max - min + 1)) + min;
+                        },
+
+        arrayOfFunctions: [ getRandomLcLetter, getRandomUcLetter, getRandomSymbol, getRandomNumber ]
+        };
+
+    
+    let yourString = '';
+    let allConditionsMet = regExes.lcRegExp.test(yourString) && regExes.ucRegExp.test(yourString) && regExes.numRegExp.test(yourString) && regExes.symbolRegExp.test(yourString);
+    
+    characters.lc.forEach(function(item) {
+        characters.uc.push(item.toUpperCase());
+        });
+ 
+//now perform the function
+        while (allConditionsMet === false) {
+            randomObject.finalArray= [];
+                for(var i = 0; i < randomObject.pwLength; i++) {
+                    let x = randomObject.randomInt(0,3);
+                    randomObject.arrayOfFunctions[x]();
+                    yourString = randomObject.finalArray.join('');
+                    allConditionsMet = regExes.lcRegExp.test(yourString) && regExes.ucRegExp.test(yourString) && regExes.numRegExp.test(yourString) && regExes.symbolRegExp.test(yourString);
+
+                };       
+            yourString = randomObject.finalArray.join('');
+            console.log(yourString);
+
+                    console.log(allConditionsMet);
+                    console.log(yourString);
+            };
+
+
+       }; //exit buildString function 
+
 		
 buildString();
 
