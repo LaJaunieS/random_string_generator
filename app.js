eb0913b16644a6754b45d702784cfe8ed705f5f4
slajaunie <stephanlajaunie@yahoo.com>
2017-02-28 12:19:20 -0800

    add html inputs and js

--- app.js
+++ app.js
@@ -5,26 +5,30 @@ console.log('script running');
 let submitButton = document.getElementById("submitButton");
 
 function onSubmit() {
+    //capture input values
+    //pass input values as parameters in build string function
+    //run build string function
     let params = {
         numberOfChars: document.getElementById("charLength").value,
         specialChars: document.getElementById("checkbox").checked
     };
     buildString(params);
-    //capture input values
-    //use as parameters in build string function
-    //run build string function
-    
-};
+    };
 
 
 function buildString(params) {
-
     let stringParams = params;
+    
     const characters = {
         lc: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ],
         uc: [],
         symbols: [ '!', '-', '^', '?', '&', '*', '#'],
     };
+    
+    
+    characters.lc.forEach(function(item) {
+        characters.uc.push(item.toUpperCase());
+        });
     
      const regExes = {
         lcRegExp: /[a-z]+/,
@@ -32,7 +36,7 @@ function buildString(params) {
         numRegExp: /[0-9]+/,
         symbolRegExp: /(\!+)|(\-+)|(\^+)|(\?+)|(\&+)|(\*+)|(\#+)/
     };
-
+    
     function getRandomLcLetter() {
             let num = randomObject.randomInt(0, characters.lc.length-1);
             let lcLetter = characters.lc[num];
@@ -64,26 +68,36 @@ function buildString(params) {
                           return Math.floor(Math.random() * (max - min + 1)) + min;
                         },
         //functions placed into array for iterating through later
-        arrayOfFunctions: [ getRandomLcLetter, getRandomUcLetter, getRandomSymbol, getRandomNumber ]
+        arrayOfFunctions: [ ]
         };
-    console.log(randomObject.pwLength);
+    
+    //test if special chars specified in order to define character array
+   if (stringParams.specialChars === true) {
+        randomObject.arrayOfFunctions = [ getRandomLcLetter, getRandomUcLetter,  getRandomNumber, getRandomSymbol  ]
+        } else {
+            randomObject.arrayOfFunctions = [ getRandomLcLetter, getRandomUcLetter,  getRandomNumber ]
+        };
     
     let yourString = '';
-    let allConditionsMet = regExes.lcRegExp.test(yourString) && regExes.ucRegExp.test(yourString) && regExes.numRegExp.test(yourString) && regExes.symbolRegExp.test(yourString);
+    let allConditionsMet = false;
     
-    characters.lc.forEach(function(item) {
-        characters.uc.push(item.toUpperCase());
-        });
+   
  
-//now perform the function
+    //now perform the function
     while (allConditionsMet === false) {
         randomObject.finalArray= [];
     
         for(var i = 0; i < randomObject.pwLength; i++) {
-            let x = randomObject.randomInt(0,3);
+            let x = randomObject.randomInt(0,randomObject.arrayOfFunctions.length-1);
             randomObject.arrayOfFunctions[x]();
             yourString = randomObject.finalArray.join('');
-            allConditionsMet = regExes.lcRegExp.test(yourString) && regExes.ucRegExp.test(yourString) && regExes.numRegExp.test(yourString) && regExes.symbolRegExp.test(yourString);
+            
+            //test if special chars specified for regExp tests
+            if (stringParams.specialChars === true) {
+                allConditionsMet = regExes.lcRegExp.test(yourString) && regExes.ucRegExp.test(yourString) && regExes.numRegExp.test(yourString) && regExes.symbolRegExp.test(yourString);
+                } else {
+                    allConditionsMet = regExes.lcRegExp.test(yourString) && regExes.ucRegExp.test(yourString) && regExes.numRegExp.test(yourString);
+                };
             };       
         
         yourString = randomObject.finalArray.join('');
